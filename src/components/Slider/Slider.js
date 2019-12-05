298596cab112d12e5345e71a654c7683903e8cac
Salamandra2050A.D <nika.krzaczek@gmail.com>
2019-12-05 14:35:47 +0100

    hooks included

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,85 +1,60 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { CSSTransitionGroup } from "react-transition-group";
 import CtrlButton from "./CtrlButton";
 import Slide from "./Slide";
 
-export default class Slider extends Component {
-  constructor(props) {
-    super(props);
+export default function Team(props) {
+  const [current, setCurrent] = useState(props.current);
+  const [next, setNext] = useState(true);
 
-    this.state = {
-      employees: this.props.employees,
-      current: this.props.current,
-      isNext: true
-    };
-
-    this.handlerPrev = this.handlerPrev.bind(this);
-    this.handlerNext = this.handlerNext.bind(this);
-  }
-
-  handlerPrev() {
-    let index = this.state.current,
-      length = this.state.employees.length;
+  const handlerPrev = () => {
+    let index = current,
+      length = props.employees.length;
 
     if (index < 1) {
       index = length;
     }
 
-    index = index - 1;
+    setCurrent(index - 1);
+    setNext(false);
+  };
 
-    this.setState({
-      current: index,
-      isNext: false
-    });
-  }
-
-  handlerNext() {
-    let index = this.state.current,
-      length = this.state.employees.length - 1;
+  const handlerNext = () => {
+    let index = current,
+      length = props.employees.length - 1;
 
     if (index === length) {
       index = -1;
     }
 
     index = index + 1;
+    setCurrent(index);
 
-    this.setState({
-      current: index,
-      isNext: true
-    });
-  }
-  componentDidMount() {
-    document.body.style.overflow = "hidden";
-  }
+    setNext(true);
+  };
+  useEffect(() => {
+    document.body.classList.add("modal-open");
+    document.title = `${props.employees[current].firstName} ${props.employees[current].lastName} - IDH ${props.employees[current].jobTitle}`;
+  });
 
-  componentWillUnmount() {
-    document.body.style.overflow = "visible";
-  }
-
-  render() {
-    let index = this.state.current,
-      isnext = this.state.isNext,
-      employee = this.state.employees[index];
-
-    return (
-      <div className="slider-backdrop">
-        <div id="slider">
-          <CSSTransitionGroup
-            transitionName={{
-              apear: false,
-              enter: isnext ? "enter-next" : "enter-prev",
-              enterActive: "enter-active",
-              leave: "leave",
-              leaveActive: isnext ? "leave-active-next" : "leave-active-prev"
-            }}
-          >
-            <Slide key={index} employee={employee} />
-          </CSSTransitionGroup>
-          <CtrlButton type="chevron-left" action={this.handlerPrev} />
-          <CtrlButton type="chevron-right" action={this.handlerNext} />
-          <CtrlButton type="times" action={this.props.handleClose} />
-        </div>
+  return (
+    <div className="slider-backdrop">
+      <div id="slider">
+        <CSSTransitionGroup
+          transitionName={{
+            apear: false,
+            enter: next ? "enter-next" : "enter-prev",
+            enterActive: "enter-active",
+            leave: "leave",
+            leaveActive: next ? "leave-active-next" : "leave-active-prev"
+          }}
+        >
+          <Slide key={current} employee={props.employees[current]} />
+        </CSSTransitionGroup>
+        <CtrlButton type="chevron-left" action={handlerPrev} />
+        <CtrlButton type="chevron-right" action={handlerNext} />
+        <CtrlButton type="times" action={props.handleClose} />
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/views/Team.js
+++ src/views/Team.js
@@ -1,59 +1,52 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Slider from "../components/Slider/Slider";
 import employees from "../employees";
 import iconMsg from "../assets/icon_msg.png";
 
-export default class Team extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      employees,
-      show: false,
-      current: 0
-    };
-  }
-  showModal = index => {
-    this.setState({ show: true, current: index });
+export default function Team() {
+  const [visibility, setVisibility] = useState(false);
+  const [current, setCurrent] = useState(0);
+  const showModal = index => {
+    setVisibility(true);
+    setCurrent(index);
   };
 
-  hideModal = () => {
-    this.setState({ show: false });
+  const hideModal = () => {
+    setVisibility(false);
   };
-  render() {
-    return (
-      <>
-        {this.state.show ? (
-          <Slider
-            handleClose={this.hideModal}
-            current={this.state.current}
-            employees={this.state.employees}
-          ></Slider>
-        ) : null}
 
-        <div className="container">
-          <h1>Meet our team</h1>
+  return (
+    <>
+      {visibility ? (
+        <Slider
+          handleClose={hideModal}
+          current={current}
+          employees={employees}
+        ></Slider>
+      ) : null}
 
-          {employees.map((employee, index) => {
-            return (
-              <div key={index}>
-                <img
-                  className="avatar"
-                  onClick={() => this.showModal(index)}
-                  src={employee.avatar}
-                  alt=""
-                />
-                <img className="mail" src={iconMsg} alt="" />
-                <p className="name">
-                  {employee.firstName + " " + employee.lastName}
-                </p>
-                <p className="job-title">{employee.jobTitle}</p>
-                <p className="place">{employee.place}</p>
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
+      <div className="container">
+        <h1>Meet our team</h1>
+
+        {employees.map((employee, index) => {
+          return (
+            <div key={index}>
+              <img
+                className="avatar"
+                onClick={() => showModal(index)}
+                src={employee.avatar}
+                alt=""
+              />
+              <img className="mail" src={iconMsg} alt="" />
+              <p className="name">
+                {employee.firstName + " " + employee.lastName}
+              </p>
+              <p className="job-title">{employee.jobTitle}</p>
+              <p className="place">{employee.place}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
